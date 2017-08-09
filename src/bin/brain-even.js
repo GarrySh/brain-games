1c48097c83971d8761e022fc6f35a5d6c58bff2a
GarrySh <sooo.lame@gmail.com>
2017-08-10 00:39:00 +1000

    Коммит не рабочий, код не дописан. Работа над 4 этапом "Проверка на четность"
    файл не завершен src/index.js

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { askUserName, showGreeting, askOddQuestions, showMessage } from '..';
+
+showMessage('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+showGreeting();
+// askOddQuestions();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+const askQuestion = (questionForUser) => {
+  const result = readlineSync.question(questionForUser);
+  return result;
 };
 
-export { getUserName as default };
+const showMessage = (message) => { console.log(message); };
+
+const askUserName = () => { askQuestion('May I have your name? '); };
+
+const showGreeting = () => { showMessage(`Hello, ${askUserName()}!\n`); };
+
+const getRandomNumber = () => {
+  const MINVALUE = 1;
+  const MAXVALUE = 39;
+  const randomNumber = (Math.random() * (MAXVALUE - MINVALUE)) + MINVALUE;
+  return Math.round(randomNumber);
+};
+
+const askOddQuestions = () => {
+  const NUMBEROFQUESTIONS = 3;
+  const currentNumber = getRandomNumber();
+  const isOddNumber = (currentNumber % 2 === 0) ? 'yes' : 'no';
+  showMessage(`Question: ${currentNumber}`);
+  const userChoice = askQuestion('Your answer: ');
+  if (userChoice === isOddNumber) {
+    showMessage('Correct!');
+  } else {
+    showMessage(`'${userChoice}' is wrong answer ;(. Correct answer was '${isOddNumber}'.\nLet's try again, ${askUserName}!`);
+  }
+};
+
+
+export { askUserName, showGreeting, askOddQuestions, showMessage };
